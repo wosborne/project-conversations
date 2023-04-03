6a5eb7c7db724143bd83f29c5c82336d93df5a0e
William Osborne <william@char.gy>
2023-04-03 07:40:05 +0100

    Add readme and seed data

--- README.md
+++ README.md
@@ -1,24 +1,69 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Task
+Use Ruby on Rails to build a project conversation history. A user should be able to:
 
-Things you may want to cover:
+- leave a comment
+- change the status of the project
 
-* Ruby version
+The project conversation history should list comments and changes in status.
 
-* System dependencies
+## Features
+* Devise user registration and authentication
+* Create a project
+* Set a projects status
+* Add a comment to a project
 
-* Configuration
+## Setup
 
-* Database creation
+Clone respository
 
-* Database initialization
+bundle install dependencies
+```
+bundle install
+```
 
-* How to run the test suite
+database setup
+```
+rake db:create db:migrate
+```
 
-* Services (job queues, cache servers, search engines, etc.)
+It's reccomended to run the database seed to provide a more testable experience.
+```
+rake db:seed
+```
 
-* Deployment instructions
+Start the rails server
+```
+rails s
+```
 
-* ...
+visit [localhost](http://localhost:3000/)
+
+This project makes use of devise authentication and the seed file provides some users to login with.
+
+`vader@deathstar.glx`\
+`tarkin@deathstar.glx`\
+`palpatine@deathstar.glx`
+
+The password for all users is `0bi-Wan@Kenobi`
+
+## Questions for colleagues
+Are there any designs available?\
+*Colleague: Here is a rough sketch on a napkin*\
+*Me: Are there any exisitng components we can use for the UI?*\
+*Colleague: Yes we have a layout, and input button components we can reuse but the rest will be new*
+
+What order should the conversation be in?\
+*Colleague: Newest at the top similar to a trello cards activity, in the future we will give users the ability to sort by their own choice*
+
+Are we expecting to add more objects to the conversation in the future?\
+*Colleague: Yes, updates to other project fields and related objects may be added to the conversation*\
+*Me: We may want to use polymorphic associations here, it may help later on if we choose to add filters along with the sorting of conversations.*
+
+## What I would do with more time
+Add styling, I left the views very barebones to focus more time on features and TDD.
+
+Tidy up test setups with improved factories.
+
+Add error handling, validations are in place to stop empty comment or status values but that is not conveyed to the user.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+vader = FactoryBot.create(:user, email: "vader@deathstar.glx", password: "0bi-Wan@Kenobi" )
+tarkin = FactoryBot.create(:user, email: "tarkin@deathstar.glx", password: "0bi-Wan@Kenobi" )
+palpatine = FactoryBot.create(:user, email: "palpatine@deathstar.glx", password: "0bi-Wan@Kenobi" )
+
+death_star = FactoryBot.create(:project, name: "Death Star")
+death_star.add_user(vader)
+death_star.add_user(tarkin)
+death_star.add_user(palpatine)
+
+# Add comments and status changes to death_star
+vaders_comment = FactoryBot.create(:comment,
+  content: "The force is strong within this project",
+  user: vader,
+  created_at: 1.year.ago
+)
+FactoryBot.create(:conversation_node, project: death_star, conversationable: vaders_comment)
+
+tarkins_comment = FactoryBot.create(:comment,
+  content: "Yes Lord Vader",
+  user: tarkin,
+  created_at: 1.year.ago + 10.minutes
+)
+FactoryBot.create(:conversation_node, project: death_star, conversationable: tarkins_comment)
+
+tarkins_status = FactoryBot.create(:status,
+  content: "Approved",
+  user: tarkin,
+  created_at: 1.year.ago + 4.days - 10.minutes
+)
+FactoryBot.create(:conversation_node, project: death_star, conversationable: tarkins_status)
+
+# Add comments and status changes to star destroyer
+star_destroyer = FactoryBot.create(:project, name: "Star Destroyer 2.0")
+star_destroyer.add_user(vader)
+star_destroyer.add_user(tarkin)
+star_destroyer.add_user(palpatine)
+
+palpatine_status = FactoryBot.create(:status,
+  content: "Planning",
+  user: tarkin,
+  created_at: 1.week.ago
+)
+FactoryBot.create(:conversation_node, project: star_destroyer, conversationable: palpatine_status)
+
+palpatine_destroyer_comment = FactoryBot.create(:comment,
+  content: "It will be even more powerful",
+  user: vader,
+  created_at: 3.days.ago - 20.minutes
+)
+FactoryBot.create(:conversation_node, project: star_destroyer, conversationable: palpatine_destroyer_comment)
+
+vaders_destroyer_comment = FactoryBot.create(:comment,
+  content: "As you wish Master",
+  user: vader,
+  created_at: 3.days.ago
+)
+FactoryBot.create(:conversation_node, project: star_destroyer, conversationable: vaders_destroyer_comment)
